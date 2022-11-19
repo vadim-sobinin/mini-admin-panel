bdc6fa58d34ba2e20caa8939f2febb3bb3a410c8
vadim-sobinin <vadimsobinin@yandex.ru>
2022-11-19 17:42:17 +0300

    debounce search

--- src/modules/searchUsers.js
+++ src/modules/searchUsers.js
@@ -1,10 +1,13 @@
 import { render } from './render';
+import { debounce } from './helpers';
 export const searchUsers = () => {
   const searchInput = document.querySelector('#search-input');
 
-  searchInput.addEventListener('input', () => {
+  const debounceSearch = debounce(() => {
     userServices.searchUsers(searchInput.value).then((users) => {
       render(users);
     });
-  });
+  }, 500);
+
+  searchInput.addEventListener('input', debounceSearch);
 };
